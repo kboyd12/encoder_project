6c8a2fe85578aa1bef468d89e89ee00ff40f5d7b
kboyd12 <kalebtboyd@comcast.net>
2020-10-04 08:47:38 -0400

    Made it look better

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import csv
 from tkinter.filedialog import asksaveasfilename, askopenfilename
 import time
 
-window = tk.Tk()
-window.title("Encoder App")
-
 
 class App():
     # Saves a new run to a file
@@ -63,42 +60,52 @@ if __name__ == "__main__":
     # Instantiates the class as an object "run"
     run = App()
 
+    window = tk.Tk()
+    window.title("Encoder App")
+    # Change background color
+    window['bg'] = '#B47676'
+
     # Creates a frame for the buttons and binds it to the sides
-    buttons_frame = tk.Frame(window)
-    buttons_frame.grid(row=0, column=0, sticky=tk.W+tk.E)
+    buttons_frame = tk.Frame(window, bg='#B47676')
+    buttons_frame.grid(row=0, column=0, sticky='ew')
 
     # Making and displaying buttons
-    btn_start_run = tk.Button(buttons_frame, text='Start Run',
-                              command=run.save_file)
+    btn_start_run = tk.Button(buttons_frame, text='Start Run', font=('calibri', 16),
+                              command=run.save_file, relief=tk.RAISED, highlightthickness=0)
     btn_start_run.grid(row=0, column=0, padx=(10), pady=10)
 
-    btn_mark = tk.Button(buttons_frame, text='Mark', command=run.mark)
+    btn_mark = tk.Button(buttons_frame, text='Mark',
+                         command=run.mark, relief=tk.RAISED, highlightthickness=0, font=('calibri', 16))
     btn_mark.grid(row=0, column=1, padx=(10), pady=10)
 
     btn_open = tk.Button(buttons_frame, text='Open File',
-                         command=run.open_file)
+                         command=run.open_file, highlightthickness=0, relief=tk.RAISED, font=('calibri', 16))
     btn_open.grid(row=0, column=2, padx=10, pady=10)
 
     # Making and displaying entry label/field
-    entry_label = tk.Label(buttons_frame, text='Entry Location')
+    entry_label = tk.Label(
+        buttons_frame, text='Entry Location', bg='#B47676', font=('calibri', 16, 'bold'))
     entry_label.grid(row=0, column=3, padx=10, pady=10)
 
-    entry_field = tk.Entry(buttons_frame)
+    entry_field = tk.Entry(
+        buttons_frame, highlightbackground='#B47676', bd=3, relief=tk.SUNKEN, highlightthickness=0)
     entry_field.grid(row=0, column=4, padx=(5), pady=10)
 
     # Run the encoder
     encoder_frame = tk.Label(buttons_frame, font=(
-        'calibri', 32, 'bold'), background='grey', fg='black')
+        'calibri', 48, 'bold'), background='grey', fg='black', bd=3)
     encoder_frame.grid(row=1, column=0, columnspan=5,
                        padx=10, pady=5, sticky='nsew')
     # Update the encoder
     run.encoder()
 
     # Create a frame for the comment box
-    group1 = tk.LabelFrame(window, text="Comments", padx=5, pady=5)
+    group1 = tk.LabelFrame(window, text="Comments", padx=5,
+                           pady=5, font=('calibri', 28, 'bold'), borderwidth=3, bg='#B47676', highlightthickness=0, relief=tk.RIDGE)
     # Create a comment box that spans 3 columns and reconfigures itself when scaled
     group1.grid(row=1, column=0, columnspan=3, padx=10,
-                pady=10, sticky=tk.E+tk.W+tk.N+tk.S)
+                pady=10, sticky='nswe')
+
     # Reconfigure when scaled
     window.columnconfigure(0, weight=1)
     window.rowconfigure(1, weight=1)
@@ -107,7 +114,8 @@ if __name__ == "__main__":
     group1.columnconfigure(0, weight=1)
 
     # Create the textbox
-    txtbox = tk.Text(group1, width=40, height=10)
-    txtbox.grid(row=0, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
+    txtbox = tk.Text(group1, width=40, height=10,
+                     font=('calibri', 16), relief=tk.SUNKEN, highlightthickness=0, bd=4)
+    txtbox.grid(row=0, column=0, sticky='nswe')
 
     window.mainloop()
